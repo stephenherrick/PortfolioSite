ec757f1ced6e8968fdcdf78df39e870077a02f9d
Stephen Herrick <stephen.herrick@gmail.com>
2019-08-26 23:08:55 +0000

    Header and footer

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,10 @@
 <template>
     <div>
+        <b-card-header 
+            header="Stephen Herrick, SDET" 
+            header-bg-variant="dark" 
+            header-text-variant="white">
+        </b-card-header>
         <b-row>
             <b-col>
             </b-col>
@@ -16,34 +21,42 @@
                         </b-button>
                     </b-col>
                 </b-row>
-                <b-row class="p-3">
-                    <b-col>
-                        <b-link href="https://linkedin.com/in/stephenpaulherick" target="_blank">
-                            <b-img style="height: 100px;" src="../assets/linkedin_logo.png" alt="LinkedIn Logo" />
-                        </b-link>
-                        <b-link href="https://github.com/stephenherrick/" target="_blank">
-                            <b-img style="height: 100px;" src="../assets/GitHub_Logo.png" alt="GitHub Logo" />
-                        </b-link>
-                    </b-col>
-                </b-row>
-                <b-row class="p-3">
+                 <b-row class="p-3">
                     <b-col>
                         <b-button block variant="outline-info" target="_blank" href="mailto:stephen.herrick@gmail.com">
                             Contact Me
                         </b-button>
                     </b-col>
                 </b-row>
+                <b-row class="p-3">
+                    <b-col>
+                        <b-link href="https://linkedin.com/in/stephenpaulherick" target="_blank">
+                            <b-img style="height: 75px;" src="../assets/linkedin_logo.png" alt="LinkedIn Logo" />
+                        </b-link>
+                        <b-link href="https://github.com/stephenherrick/" target="_blank">
+                            <b-img style="height: 75px;" src="../assets/GitHub_Logo.png" alt="GitHub Logo" />
+                        </b-link>
+                    </b-col>
+                </b-row>
             </b-col>
             <b-col>
             </b-col>
         </b-row>
+        <b-card-footer :footer=getFooterText>
+        </b-card-footer>
     </div>
 </template>
 
 <script>
     export default {
-
-    }
+        computed:{
+            getFooterText:function(){
+                var year = new Date().getFullYear().toString();
+                var footerText = "Copyright  ©"+year+" stephenherrick.dev";
+                return footerText
+        }
+    }        
+}
 </script>
 
 <style>
