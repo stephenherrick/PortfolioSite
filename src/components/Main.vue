ba4ed9842637c59f860b8ded52d21a730dec2263
Stephen Herrick <stephen.herrick@gmail.com>
2019-08-27 21:26:50 +0000

    Click

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -18,7 +18,7 @@
                     <b-col>
                         <b-button block variant="outline-info" target="_blank"
                             href="https://drive.google.com/file/d/0BzmBR3K6IccHaUFBazJocG1lSXc/view?usp=sharing"
-                            @onclick="captureOutboundLink('https://drive.google.com/file/d/0BzmBR3K6IccHaUFBazJocG1lSXc/view?usp=sharing');">Resume
+                            @click="captureOutboundLink('https://drive.google.com/file/d/0BzmBR3K6IccHaUFBazJocG1lSXc/view?usp=sharing');">Resume
                         </b-button>
                     </b-col>
                 </b-row>
@@ -26,17 +26,17 @@
                     <b-col>
                         <b-button block variant="outline-info" target="_blank" 
                             href="mailto:stephen.herrick@gmail.com"
-                            @onclick="captureOutboundLink('mailto:stephen.herrick@gmail.com');">
+                            @click="captureOutboundLink('mailto:stephen.herrick@gmail.com');">
                             Contact Me
                         </b-button>
                     </b-col>
                 </b-row>
                 <b-row class="p-2">
                     <b-col>
-                        <b-link href="https://linkedin.com/in/stephenpaulherrick" target="_blank" @onclick="captureOutboundLink('https://linkedin.com/in/stephenpaulherrick');">
+                        <b-link href="https://linkedin.com/in/stephenpaulherrick" target="_blank" @click="captureOutboundLink('https://linkedin.com/in/stephenpaulherrick');">
                             <b-img style="height: 75px; padding-right: 25px;" src="../assets/linkedin_logo.png" alt="LinkedIn Logo" />
                         </b-link>
-                        <b-link href="https://github.com/stephenherrick/" target="_blank" @onclick="captureOutboundLink('https://github.com/stephenherrick/');">
+                        <b-link href="https://github.com/stephenherrick/" target="_blank" @click="captureOutboundLink('https://github.com/stephenherrick/');">
                             <b-img style="height: 75px; padding-left: 25px;" src="../assets/GitHub_Logo.png" alt="GitHub Logo" />
                         </b-link>
                     </b-col>
